c7e50f881f73a6c8dfbf11a377174063af9dc87a
OlgaYurch <volha@yurchanka>
2023-12-20 22:00:37 +0300

    feat: pass task toPrecision

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -372,7 +372,7 @@ function toFixed(/* number, fractionDigits */) {
 }
 
 /**
- * -!!! DON'T PASS !!!- Returns a string representation of a number in normal (fixed-point or exponential)
+ * ++ Returns a string representation of a number in normal (fixed-point or exponential)
  * notation rounded to precision significant digits.
  *
  * @param {number} number
@@ -383,8 +383,8 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
